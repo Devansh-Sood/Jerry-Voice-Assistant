43c9308ddf6cbc569447dfadd0ddcea208a15a50
Devansh Sood <devanshsood.ss@gmail.com>
2020-02-29 02:15:35 +0530

    voice assistant till wiki

---
+++ Jerry.py
@@ -0,0 +1,60 @@
+import pyttsx3
+import datetime
+import speech_recognition as sr
+import wikipedia
+
+
+engine = pyttsx3.init('sapi5')      # we can use the inbuilt voices from the windows with this
+voices=engine.getProperty('voices')
+# print(voices[0].id)
+engine.setProperty('voice',voices[0].id)     # to set the voice which we want to use
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour >=0 and hour <12:
+        speak("Good Morning Mr. Sood")
+
+    elif hour >=12 and hour <18:
+        speak("Good Afternoon Mr. Sood")
+
+    else :
+        speak("Good Evening Mr. Sood") 
+
+    speak("I am Jerry , How May I Assist You ?")
+
+def takeCommand():
+
+    r = sr.Recognizer()         #helps to recognize audio
+    with sr.Microphone() as source:
+        print("Listening....")
+        r.pause_threshold = 1            # seconds of non-speaking audio before a phrase is considered complete
+        audio = r.listen(source)
+    
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio , language='en-in')            # attempt to recognize any speech in the audio
+        print("User said: ", query)
+    except Exception :
+        # print(e)
+        print("Can you repeat please...")
+        return "None"
+    return query
+
+if __name__=="__main__":        # main method
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        # logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=3)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
