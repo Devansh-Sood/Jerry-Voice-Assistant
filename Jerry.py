8cf28af8598cbb11ef1eb3d28a4fe3129c5d7b28
Devansh Sood <devanshsood.ss@gmail.com>
2020-03-01 16:59:54 +0530

    webbrowser module and play music

--- Jerry.py
+++ Jerry.py
@@ -2,6 +2,9 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import webbrowser
+import os
+import random
 
 
 engine = pyttsx3.init('sapi5')      # we can use the inbuilt voices from the windows with this
@@ -58,3 +61,18 @@ if __name__=="__main__":        # main method
             speak("According to Wikipedia")
             print(results)
             speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'play music' in query:
+            music_dir = 'E:\\movies\\SONGS'         #giving the path of songs file in the system
+            songs = os.listdir(music_dir)           #list all the files in the music_dir
+            print(songs)    
+            os.startfile(os.path.join(music_dir, random.choice(songs)))         #opens the file and plays the random song
